86bfcc43ab0cec4e9c7a99d4e20aadcacc597e67
Alan Arsiniega <arsiniega@gmail.com>
2020-11-13 20:17:19 -0500

    Created java project with Maven

---
+++ csis656-uber/src/main/java/edu/cofc/csis656/uber/App.java
@@ -0,0 +1,13 @@
+package edu.cofc.csis656.uber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ csis656-uber/src/test/java/edu/cofc/csis656/uber/AppTest.java
@@ -0,0 +1,20 @@
+package edu.cofc.csis656.uber;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
